145c8a6a72f2093741b78cae37274d9c6a2f3b5c
Zach Friedman <zif@umd.edu>
2018-04-05 23:26:16 +0000

    moved a bunch of the clasp related steps to the /dist folder as that is really all we should be pushing to script.google.com (the webpack combined/global var file)

--- src/index2.js
+++
@@ -1,8 +0,0 @@
-/**
- * index two
- */
-export const helloWorld = () => {
-  Logger.log('Hello world!');
-};
-
-global.hello1 = helloWorld;
--- src/index3.js
+++
@@ -1,9 +0,0 @@
-/**
- * index three
- */
-export const helloWorld2 = () => {
-  Logger.log('Hello world2!!!!');
-};
-
-
-global.hello2 = helloWorld2;
