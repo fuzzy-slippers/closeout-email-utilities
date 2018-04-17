ec1d51d5f1ad52c221726a11df61c15dc742fb68
Zach Friedman <zif@umd.edu>
2018-04-17 18:18:55 +0000

    got clasp push working again - had been giving error complaining about gitignore and file enums, but I think it was a property I tried to add to specify the clasp directory to upload (dist directory path) in any case, I was able to do a build and push and run the test program through the apps script console afterward and it was querying via alasql correctly

--- src/include-in-webpack.js
+++ src/include-in-webpack.js
@@ -3,11 +3,6 @@ const jsUtils = require("./js-utils.js");
 //add the imported modules to global, just like normal (except with this manual step - all subfunctions should be accessible)
 global.jsUtils = jsUtils;
 
-
-//  const testUsingLodash = require("./test-using-lodash.js");
-//  global.testUsingLodash = testUsingLodash;
-/**End of include-in-webpack.js**/
-
 const alasqlUtils = require("./alasql-utils.js");
 global.alasqlUtils = alasqlUtils;
 /**End of include-in-webpack.js**/
--- src/test-using-lodash.js
+++
@@ -1,8 +0,0 @@
-/**Start of test-using-alasql.js**/
-const lodash = require("lodash");
-global.lodash = lodash;
-
-function testAlasqlWorking()
-{
-    return lodash.random(10, 40);
-}
