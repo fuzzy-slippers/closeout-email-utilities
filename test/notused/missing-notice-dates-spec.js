3da124d42f5ac6454da70707ebf1dfa750a52a01
Zach Friedman <zif@umd.edu>
2018-09-19 01:05:17 +0000

    Created initial framework for testing missing-notice-dates module updateArrDataAddAdditionalFlaggedEmptyTimeAndMoneyNoticeDates function including an initial rewire mock of the latestByPrimaryKey findMaxPrimaryKeyValueInData library so that if passed in an empty array as the sheet data, that it does a mock up that just returns null (as if the google property cache had no record for the last primary key value stored or the properties store got erased or something) created the first test that verifies that if you pass in blank data and it cant find a max primary key in the properties store, then it will just return back an empty array

--- test/notused/missing-notice-dates-spec.js
+++ test/notused/missing-notice-dates-spec.js
@@ -1,169 +1,37 @@
-// /*jshint curly:true, debug:true esversion:6 strict:true undef:true unused:true varstmt:true mocha:true node:true */
-// //not fully working...got overwhelmed with the number of sub functions and GAS cache values that would need to be mocked out and that sinon spy's would be needed to test if the spreadsheet was going to be updated with the correct data
-// require("mocha");
-// const should = require("should");
-// const rewire = require("rewire");
+/*jshint curly:true, debug:true esversion:6 strict:true undef:true unused:true varstmt:true mocha:true node:true */
+//not fully working...got overwhelmed with the number of sub functions and GAS cache values that would need to be mocked out and that sinon spy's would be needed to test if the spreadsheet was going to be updated with the correct data
+require("mocha");
+const should = require("should");
+const rewire = require("rewire");
 
-// const missingNoticeDates = rewire("../../src/time-and-money/missing-notice-dates.js");
-// // // using rewire to mock out the responses from the callKrGetApiWithWait function called inside the missing-notice-dates module - use switch statement to return json for known primary keys and otherwise return json example when an error occurs
-// // const apiUtils = rewire("../src/api-utils.js");
-// // apiUtils.__set__("googleAppsScriptWrappers", {
-// //     readDataInSheetWHeaderRowByName: function (relativeApiUri) {
-// //                                                           // console.log(`inside rewiring of callKrGetApiWithWait, relativeApiUri detected as: ${relativeApiUri}`)      
-// //       switch (relativeApiUri) {
-// //       case "/award/api/v1/award-amount-transactions/" + "773750":
-// //         return '{"awardAmountTransactionId":773750,"comments":null,"documentNumber":"2455782","noticeDate":1525233600000,"awardNumber":"029053-00001","transactionTypeCode":4,"_primaryKey":"773750"}';
-// //       case "/award/api/v1/award-types/":
-// //         return '[{"code":1,"description":"Grant","_primaryKey":"1"},{"code":3,"description":"Contract","_primaryKey":"3"},{"code":4,"description":"Indefinite Delivery Contract","_primaryKey":"4"},{"code":5,"description":"Cooperative Agreement","_primaryKey":"5"},{"code":8,"description":"Consortium Membership","_primaryKey":"8"},{"code":9,"description":"Other Transaction Agreement","_primaryKey":"9"},{"code":13,"description":"Non Disclosure Agreement","_primaryKey":"13"},{"code":14,"description":"Material Transfer Agreement","_primaryKey":"14"},{"code":15,"description":"Teaming Agreement","_primaryKey":"15"},{"code":16,"description":"Equipment Loan","_primaryKey":"16"},{"code":17,"description":"Memorandum of Understanding","_primaryKey":"17"},{"code":18,"description":"Intergovernmental Personnel Assignment","_primaryKey":"18"},{"code":19,"description":"CHOOSE AN AWARD TYPE - was blank in RAA - DO NOT USE","_primaryKey":"19"},{"code":20,"description":"Data Use Agreement","_primaryKey":"20"},{"code":21,"description":"Software License","_primaryKey":"21"}]';
-// //       default:
-// //         return '{"Error":{"errors":["not found for key -1"]}}';
-// //       }
-// //     }
-// // });
-// describe("missing-notice-dates", function() {
+ const missingNoticeDates = rewire("../../src/time-and-money/missing-notice-dates.js");
+// using rewire to mock out the responses from the callKrGetApiWithWait function called inside the missing-notice-dates module - use switch statement to return json for known primary keys and otherwise return json example when an error occurs
+const latestByPrimaryKey = rewire("../../src/latest-by-primary-key.js");
+missingNoticeDates.__set__("latestByPrimaryKey", {
+    findMaxPrimaryKeyValueInData: function (colToFindMax, twoDimArrWHeader, relativeUriPath) {
+                                                          // console.log(`inside rewiring of callKrGetApiWithWait, relativeApiUri detected as: ${relativeApiUri}`)      
+      switch (twoDimArrWHeader) {
+      case (twoDimArrWHeader.length === 0):
+        return null;
+      default:
+        return '{"Error":{"errors":["not found for key -1"]}}';
+      }
+    },
+    
+    gatherAdditionalRowsBasedOnTryingApiCallsWithIncreasingPrimaryKeys: (maxPreviouslyUsedPrimaryKey, relativeUriPath) => latestByPrimaryKey.gatherAdditionalRowsBasedOnTryingApiCallsWithIncreasingPrimaryKeys(maxPreviouslyUsedPrimaryKey, relativeUriPath)
+});
+describe("missing-notice-dates", function() {
   
-//   describe("#addAdditionalFlaggedEmptyTimeAndMoneyNoticeDatesToSheet()", function() {
-//     it("should when the sheet is empty with no header rows (and a mocked last primary key value that points to empty data), set the sheet to empty array", function () {
-//       objUtils.prependAllObjKeys({"key1": 0}, "add-to-left-side.").should.be.eql({"add-to-left-side.key1": 0});
-//     }); 
+  describe("#updateArrDataAddAdditionalFlaggedEmptyTimeAndMoneyNoticeDates()", function() {
+    it("should when the sheet is empty with no header rows (and a mocked last primary key value that points to empty data), set the sheet to empty array", function () {
+      missingNoticeDates.updateArrDataAddAdditionalFlaggedEmptyTimeAndMoneyNoticeDates([]).should.be.eql([]);
+    }); 
+  
     
-//     it("should given an object with multiple properties {key1: 0, key2: 1} and the string to add 'appendThis' return back {appendThiskey1: 0, , appendThiskey2: 1}", function () {
-//       objUtils.prependAllObjKeys({key1: "A", key2: "B"}, "appendThis").should.be.eql({appendThiskey1: "A", appendThiskey2: "B"});
-//     });    
-    
-//     it("should given an empty object return an empty object", function () {
-//       objUtils.prependAllObjKeys({}, "doesNotMatter").should.be.eql({});
-//     });  
-    
-//     it("should given undefined return undefined", function () {
-//       should.equal(objUtils.prependAllObjKeys(undefined, "doesNotMatter"), undefined);
-//     });   
+  });
+  
 
-//     it("should if given an error object (object with Error property) return that error object unchanged", function () {
-//       objUtils.prependAllObjKeys({"Error":{"errors":["some error"]}}, "doesNotMatter").should.be.eql({"Error":{"errors":["some error"]}});
-//     });    
-    
-//   });
-  
-//   describe("#prependAllArrOfObjKeys()", function() {
-//     it("should given a standalone object (not in an array) with a single key {'A': 0} and the string to add 'entity.' return back {'entity.A': 0}", function () {
-//       objUtils.prependAllArrOfObjKeys({"A": 0}, "entity.").should.be.eql({"entity.A": 0});
-//     }); 
-    
-//     it("should given an array with just a single object inside with multiple object key value pairs should return back the single object in the array but with the property names prepended", function () {
-//       objUtils.prependAllArrOfObjKeys([{"A": 0, "B": 1}], "award-unit.").should.be.eql([{"award-unit.A": 0, "award-unit.B": 1}]);
-//     });   
-
-//     it("should given an array with three objects inside some with one property and some with multiple properties should append the string passed in to the left side of all property names", function () {
-//       objUtils.prependAllArrOfObjKeys([{"prop1": null},{prop1: "A", prop2: "B"}, {"otherProp1": 1, "otherProp2": 2}], "prepend-this.")
-//       .should.be.eql([{"prepend-this.prop1": null},{"prepend-this.prop1": "A", "prepend-this.prop2": "B"}, {"prepend-this.otherProp1": 1, "prepend-this.otherProp2": 2}]);
-//     }); 
-
-//     it("should given an array of objects (example based on award-types) return back an array with each object having the property names prepended/prefaced with the endpoint name", function () {
-//       objUtils.prependAllArrOfObjKeys(
-//         [
-//         {"code":1,"description":"Grant","_primaryKey":"1"},
-//         {"code":3,"description":"Contract","_primaryKey":"3"},
-//         {"code":4,"description":"Indefinite Delivery Contract","_primaryKey":"4"},
-//         {"code":5,"description":"Cooperative Agreement","_primaryKey":"5"},
-//         {"code":8,"description":"Consortium Membership","_primaryKey":"8"},
-//         {"code":9,"description":"Other Transaction Agreement","_primaryKey":"9"},
-//         {"code":13,"description":"Non Disclosure Agreement","_primaryKey":"13"},
-//         {"code":14,"description":"Material Transfer Agreement","_primaryKey":"14"},
-//         {"code":15,"description":"Teaming Agreement","_primaryKey":"15"},
-//         {"code":16,"description":"Equipment Loan","_primaryKey":"16"},
-//         {"code":17,"description":"Memorandum of Understanding","_primaryKey":"17"},
-//         {"code":18,"description":"Intergovernmental Personnel Assignment","_primaryKey":"18"},
-//         {"code":19,"description":"CHOOSE AN AWARD TYPE - was blank in RAA - DO NOT USE","_primaryKey":"19"},
-//         {"code":20,"description":"Data Use Agreement","_primaryKey":"20"},
-//         {"code":21,"description":"Software License","_primaryKey":"21"}
-//         ],
-//         "award-types.")
-//       .should.be.eql(
-//         [
-//         {"award-types.code":1,"award-types.description":"Grant","award-types._primaryKey":"1"},
-//         {"award-types.code":3,"award-types.description":"Contract","award-types._primaryKey":"3"},
-//         {"award-types.code":4,"award-types.description":"Indefinite Delivery Contract","award-types._primaryKey":"4"},
-//         {"award-types.code":5,"award-types.description":"Cooperative Agreement","award-types._primaryKey":"5"},
-//         {"award-types.code":8,"award-types.description":"Consortium Membership","award-types._primaryKey":"8"},
-//         {"award-types.code":9,"award-types.description":"Other Transaction Agreement","award-types._primaryKey":"9"},
-//         {"award-types.code":13,"award-types.description":"Non Disclosure Agreement","award-types._primaryKey":"13"},
-//         {"award-types.code":14,"award-types.description":"Material Transfer Agreement","award-types._primaryKey":"14"},
-//         {"award-types.code":15,"award-types.description":"Teaming Agreement","award-types._primaryKey":"15"},
-//         {"award-types.code":16,"award-types.description":"Equipment Loan","award-types._primaryKey":"16"},
-//         {"award-types.code":17,"award-types.description":"Memorandum of Understanding","award-types._primaryKey":"17"},
-//         {"award-types.code":18,"award-types.description":"Intergovernmental Personnel Assignment","award-types._primaryKey":"18"},
-//         {"award-types.code":19,"award-types.description":"CHOOSE AN AWARD TYPE - was blank in RAA - DO NOT USE","award-types._primaryKey":"19"},
-//         {"award-types.code":20,"award-types.description":"Data Use Agreement","award-types._primaryKey":"20"},
-//         {"award-types.code":21,"award-types.description":"Software License","award-types._primaryKey":"21"}
-//         ]);
-//     });     
-    
-//     //
-//     it("should given an empty array return an empty array", function () {
-//       objUtils.prependAllArrOfObjKeys({}, "doesNotMatter").should.be.eql({});
-//     });  
-    
-//     it("should given an empty object not in an array return that empty object", function () {
-//       objUtils.prependAllArrOfObjKeys({}, "doesNotMatter").should.be.eql({});
-//     });  
-    
-//     it("should given an object with just an empty object inside return that same array with the empty object", function () {
-//       objUtils.prependAllArrOfObjKeys({}, "doesNotMatter").should.be.eql({});
-//     });      
-    
-//     it("should given undefined return undefined", function () {
-//       should.equal(objUtils.prependAllArrOfObjKeys(undefined, "doesNotMatter"), undefined);
-//     });    
-    
-//     it("should if given an error object (object with Error property) return that error object unchanged", function () {
-//       objUtils.prependAllArrOfObjKeys({"Error":{"errors":["some error"]}}, "doesNotMatter").should.be.eql({"Error":{"errors":["some error"]}});
-//     });     
-    
-//   });  
   
   
-  
-//   describe("#mergeObjArrIntoSingleObj()", function() {
-//     it("should if passed an array with two objects, the first with a property A and the second with a property B, should return a single object with both properties", function () {
-//       objUtils.mergeObjArrIntoSingleObj([{A:1}, {B:2}]).should.be.eql({A:1,B:2});
-//     }); 
-    
-//     it("should if passed an array with three objects, each having 2 properties A&B, C&D, E&F, should return a single object with all 6 properties", function () {
-//       objUtils.mergeObjArrIntoSingleObj([{A:1,B:2},{C:3,D:4},{E:5,F:6}]).should.be.eql({A:1,B:2,C:3,D:4,E:5,F:6});
-//     });     
-  
-//     it("should if passed an array with two objects, both with properties Z, return one object with property Z and the value from the second (right most) object in the array", function () {
-//       objUtils.mergeObjArrIntoSingleObj([{Z:1}, {Z:100}]).should.be.eql({Z:100});
-//     });   
-  
-//     it("should if passed an array with two objects, the first with properties A&B and the second with a properties B&C, should return a single object with props AB and C with the value for B coming from the object later (higher index) in the array", function () {
-//       objUtils.mergeObjArrIntoSingleObj([{A:1,B:2}, {B:3,C:4}]).should.be.eql({A:1,B:3,C:4});
-//     });  
-    
-//     it("should if passed an empty array return an empty object", function () {
-//       objUtils.mergeObjArrIntoSingleObj([]).should.be.eql({});
-//     });  
-    
-//     it("should if passed undefined return undefined", function () {
-//       should.equal(objUtils.mergeObjArrIntoSingleObj(undefined), undefined);
-//     }); 
-    
-//     // may want to include case of what to do when an object not in an array is passed in
-    
-//   });
-  
-//   describe("#objectAssignSingleObjEs3Friendly()", function() {
-//     it("should if passed an two objects, the first with a property A and the second with a property B, should return a single object with both properties", function () {
-//       objUtils.objectAssignSingleObjEs3Friendly({A:1}, {B:2}).should.be.eql({A:1,B:2});
-//     }); 
-  
-//     it("should if passed two objects, both with properties Z, return one object with property Z and the value from the second (right most) object passed in", function () {
-//       objUtils.objectAssignSingleObjEs3Friendly({Z:1}, {Z:100}).should.be.eql({Z:100});
-//     });   
-    
-//   });  
-  
-  
-// });
+});
 
