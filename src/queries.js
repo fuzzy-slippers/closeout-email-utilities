c8746d39227a7561b8480bbbf87f48ad31ccac6e
Zach Friedman <zif@umd.edu>
2018-10-06 22:51:13 +0000

    Switched all the calls in   unionUsingFirstTablePrimaryKeyExtraColumnsInFirstTablePreservedUnsorted over to generateListOfColumnNamesInAlaSqlSelectFormatFirstNonEmptyTwoDArr and all tests still passing

--- src/queries.js
+++ src/queries.js
@@ -65,13 +65,8 @@ module.exports = {
     const thirdTwoDArrWHeaderEmptyArrIfNotPassedIn = (thirdTwoDArrWHeader.length > 0 ? thirdTwoDArrWHeader : []);    
     //due to a bug in AlaSQL that causes the second and third tables in a union statement to be blank if using SELECT *, but not if you list out the column names, had to do this dynamically based on the column names in the first 2d array and converting them with join to format 'col1', 'col2', etc - 
     // however if data for that table is an empty array, we needed to get the column name list from somewhere else (using * causes the unions to return blank rows) so found that can use the header rows from the second or third table data instead as long as those are not blank
-    const columnNamesFirstTable =   (twoDArrWHeader.length > 0 ? module.exports.generateListOfColumnNamesInAlaSqlSelectFormat(twoDArrWHeader) : 
-                                                              secondTwoDArrWHeader.length > 0 ? module.exports.generateListOfColumnNamesInAlaSqlSelectFormat(secondTwoDArrWHeader) : 
-                                                              module.exports.generateListOfColumnNamesInAlaSqlSelectFormat(thirdTwoDArrWHeader)
-                                    );
-    const columnNamesSecondTable = (secondTwoDArrWHeader.length > 0 ? module.exports.generateListOfColumnNamesInAlaSqlSelectFormat(secondTwoDArrWHeader): 
-                                                              module.exports.generateListOfColumnNamesInAlaSqlSelectFormat(thirdTwoDArrWHeader) 
-                                    );
+    const columnNamesFirstTable = module.exports.generateListOfColumnNamesInAlaSqlSelectFormatFirstNonEmptyTwoDArr(twoDArrWHeader, secondTwoDArrWHeader, thirdTwoDArrWHeader);
+    const columnNamesSecondTable = module.exports.generateListOfColumnNamesInAlaSqlSelectFormatFirstNonEmptyTwoDArr(secondTwoDArrWHeader, thirdTwoDArrWHeader);
 
     ////attempting to simplify
     // const columnNamesFirstTable =   (twoDArrWHeader.length > 0 ? module.exports.generateListOfColumnNamesInAlaSqlSelectFormat(twoDArrWHeader) : 
@@ -167,7 +162,7 @@ module.exports = {
   * @param {string[][]} a 2d array with a header row and potentially data (to use as the basis for the column name list) 
   * @return {string} a string in the valid '[col1], [col2], [col3]' format for alasql SELECT statements (based on the first passed in non-empty 2d arr) - note that brackets are needed to handle column names with dot notation and other special characters and cant hurt - returns '*' if empty array passed in
   */    
-  generateListOfColumnNamesInAlaSqlSelectFormatFirstNonEmptyTwoDArr: (twoDArrWHeader1, twoDArrWHeader2, twoDArrWHeader3) => {
+  generateListOfColumnNamesInAlaSqlSelectFormatFirstNonEmptyTwoDArr: (twoDArrWHeader1 = [], twoDArrWHeader2 = [], twoDArrWHeader3 = []) => {
     log.trace(`queries generateListOfColumnNamesInAlaSqlSelectFormatFirstNonEmptyTwoDArr: (${JSON.stringify(twoDArrWHeader1)}, ${JSON.stringify(twoDArrWHeader2)}, ${JSON.stringify(twoDArrWHeader3)}) called...`);
     if (twoDArrWHeader1.length > 0 && twoDArrWHeader1[0].length > 0)
       return module.exports.generateListOfColumnNamesInAlaSqlSelectFormat(twoDArrWHeader1);
